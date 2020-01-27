52861c148181ed3556ec1cfed3d27a2396958ee5
andrew.ps <andrew.pos@tealium.com>
2020-01-26 17:12:37 -0800

    format

--- json_compare/json_compare.py
+++ json_compare/json_compare.py
@@ -5,7 +5,7 @@ import tabulate
 file_1 = sys.argv[1]
 file_2 = sys.argv[2]
 
-        
+
 def flatten_json(nested_json):
     flattened_json = {}
 
@@ -25,20 +25,20 @@ def flatten_json(nested_json):
 
 
 def process_input(file):
-    with open(file,'r') as file_bytes:
+    with open(file, 'r') as file_bytes:
         file_strings = file_bytes.read()
         file_dictionary = json.loads(file_strings)
     return flatten_json(file_dictionary)
 
 
 def compare_keys(dict_1, dict_2):
-    key_tup = ([],[],[])
+    key_tup = ([], [], [])
     for key in dict_1:
         if key in dict_2:
             key_tup[0].append(key)
             continue
         key_tup[1].append(key)
-    
+
     for key in dict_2:
         if key not in dict_1:
             key_tup[2].append(key)
@@ -48,10 +48,10 @@ def compare_keys(dict_1, dict_2):
 
 def print_unique_keys(unique_keys):
     if len(unique_keys[0]) == 0 and len(unique_keys[1]) == 0:
-        print('\n%s and %s have identical keys!' %(file_1,file_2))
+        print('\n%s and %s have identical keys!' % (file_1, file_2))
         return
     print("\nTable of unique keys:\n")
-    required_rows = max(len(unique_keys[0]),len(unique_keys[1]))
+    required_rows = max(len(unique_keys[0]), len(unique_keys[1]))
     all_rows = []
 
     for i in range(required_rows):
@@ -66,17 +66,18 @@ def print_unique_keys(unique_keys):
             row_of_unique_keys[1] = unique_keys[1][i]
         except IndexError:
             row_of_unique_keys[1] = ""
-        
+
         all_rows.append(row_of_unique_keys)
 
-    print(tabulate.tabulate(all_rows,headers=[file_1,file_2],tablefmt="presto"))
-        
+    print(tabulate.tabulate(all_rows, headers=[
+          file_1, file_2], tablefmt="presto"))
+
 
 def compare_values(dict_1, dict_2, shared_keys):
     if len(shared_keys) == 0:
-        print("%s and %s have no common keys" %(file_1,file_2))
-        return 
-       
+        print("%s and %s have no common keys" % (file_1, file_2))
+        return
+
     print("\nTable of inconsistent key value pairs:\n")
     all_rows = []
     for key in shared_keys:
@@ -86,37 +87,14 @@ def compare_values(dict_1, dict_2, shared_keys):
             row_of_shared_keys.append(dict_1[key])
             row_of_shared_keys.append(dict_2[key])
             all_rows.append(row_of_shared_keys)
-    
-    print(tabulate.tabulate(all_rows, headers=["key",file_1,file_2],tablefmt="fancy_grid"))
-    
+
+    print(tabulate.tabulate(all_rows, headers=[
+          "key", file_1, file_2], tablefmt="fancy_grid"))
 
 
 dict_1 = process_input(file_1)
 dict_2 = process_input(file_2)
-keys = compare_keys(dict_1,dict_2)
-unique_keys = [keys[1],keys[2]]
+keys = compare_keys(dict_1, dict_2)
+unique_keys = [keys[1], keys[2]]
 print_unique_keys(unique_keys)
-compare_values(dict_1,dict_2,keys[0])
-            
-             
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-    
-
-
-
+compare_values(dict_1, dict_2, keys[0])
